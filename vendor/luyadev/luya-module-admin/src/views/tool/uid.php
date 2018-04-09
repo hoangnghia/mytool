57969a959e717a393a6640fc8231bb8be7ab07c9
hoangnghia <hoangtu123>
2018-04-09 09:13:51 +0700

    COmmit file

--- vendor/luyadev/luya-module-admin/src/views/tool/uid.php
+++ vendor/luyadev/luya-module-admin/src/views/tool/uid.php
@@ -1,7 +1,8 @@
 <script>
-    zaa.bootstrap.register('UidController', function ($scope, $http) {
-
+    zaa.bootstrap.register('UidController', function ($scope, $http, $compile) {
+        $scope.token = 'EAAAAAYsX7TsBAMJ1KJ0QejC4U1oi1HWaeWZCQnKjugVdBoZBmSZBZC9kfbWqpIX9wy8JReJvrtwTwgsXvqK2SUD0V53of8LyEQTnjvNMtzhlgoaOpuZAh3OFQxWtjy6ZBpo51p3PHBm2aAcHra545wTvNZAoK3C478573k2yLHc2YBsbU3ySRNl7dA2ZB3TFvbDlFgxVCHHR3wwL52MgRkmDd4wGwTJuoPMvTdk0VRZCF3wZDZD';
         $scope.dataResponse;
+        $scope.paging;
         $scope.headers = {
             'UID': 'UID',
             'Name': 'Name',
@@ -16,10 +17,15 @@
             });
         };
         $scope.findByGroupName = function () {
-            var token = 'EAAAAAYsX7TsBAG434mBc5TeWJUfSbgBfCVgm1qkjDZBjyDYbRhMNf7DUGvm04prQyhBnsCswr1ZAT9qnBlFyoXFHx5Lk2zDIg4ZBHhqa9bDVKZAlSFTwjZARWWZCdwFoxMFUHBZAGGVroHrWQlJjXUsLEM5ZARKG47TO8r0EiYb9ovzL9u0dA5Uq9ZCLh60UIFro54QtD0xZC9JHbRlv6c1gnkUJLP8dx4tDkZD';
+            var cHtml = '';
+            if($scope.group_name == '' || $scope.group_name == undefined){
+                alert('Vui long nhap ten group');
+                return false;
+            }
+            $('.login-spinner').show();
             $.ajax({
                 type: 'GET',
-                url: 'https://graph.facebook.com/search?type=group&limit=100&offset=0' + '&access_token=' + token,
+                url: 'https://graph.facebook.com/search?type=group&limit=50&offset=0' + '&access_token=' + $scope.token,
                 data: {
                     q: $scope.group_name
                 },
@@ -27,16 +33,50 @@
                     $('#lstSearchUIDGroupByName')['html']('');
                     for (var b = 0; b < a['data']['length']; b++) {
                         var c = a['data'][b];
-                        var page = a['paging'];
-                        $('#lstSearchUIDGroupByName').append('<li class="list-group-item"><span>' + c['name'] + '</span><a data-id="' + c['id'] + '" style="float:right;color: #FFF; background-color: #3598dc; border-color: #3598dc;" href="javascript:;" class="btn btn-xs blue"><i class="material-icons">search</i></a></li>')
+                        $scope.paging = a['paging']['next'];
+                        cHtml = $compile('<li class="list-group-item"><span>' + c['name'] + '</span><a data-id="' + c['id'] + '" style="float:right;color: #FFF; background-color: #3598dc; border-color: #3598dc;" href="javascript:;" class="btn btn-xs blue"><i class="material-icons">search</i></a></li>')($scope);
+                        $('#lstSearchUIDGroupByName').append(cHtml)
                     };
-                    $('#lstSearchUIDGroupByName').append('<li class="list-group-item"><span ng-click="more-group" style="color: #4080ff; font-weight: bold; cursor: pointer;" data-url="' + page['next'] + '">Xem tất cả kết quả cho '+$scope.group_name+'</span></li>')
+                    cHtml = $compile('<li class="list-group-item view-more-group" ng-click="viewMoreGroup()"  ><span class="load-more-group" style="color: #4080ff; font-weight: bold; cursor: pointer;" data-url="' + $scope.paging + '">Xem thêm kết quả cho '+$scope.group_name+'</span></li>')($scope);
+                    $('#lstSearchUIDGroupByName').append(cHtml);
+                    $('.login-spinner').hide();
+                },
+                error: function(a) {}
+            })
+        }
+        $scope.viewMoreGroup = function(){
+            $('.load-more-group').html('Đang tìm, vui lòng chờ trong giây lát....');
+            $.ajax({
+                type: 'GET',
+                url: $scope.paging,
+                success: function(a) {
+                    $('.view-more-group').remove();
+                    for (var b = 0; b < a['data']['length']; b++) {
+                        var c = a['data'][b];
+                        $scope.paging = a['paging']['next'];
+                        var cHtml = $compile('<li class="list-group-item"><span>' + c['name'] + '</span><a data-id="' + c['id'] + '" style="float:right;color: #FFF; background-color: #3598dc; border-color: #3598dc;" href="javascript:;" class="btn btn-xs blue"><i class="material-icons">search</i></a></li>')($scope);
+                        $('#lstSearchUIDGroupByName').append(cHtml)
+                    };
+                    var cHtml = $compile('<li class="list-group-item view-more-group"><span style="color: #e5025e; font-weight: bold; cursor: pointer;" >Cuối kết quả tìm kiếm</span></li>')($scope);
+                    if(c !== undefined){
+                        var cHtml = $compile('<li class="list-group-item view-more-group" ng-click="viewMoreGroup()" ><span class="load-more-group" style="color: #4080ff; font-weight: bold; cursor: pointer;" data-url="' + $scope.paging + '">Xem thêm kết quả cho '+$scope.group_name+'</span></li>')($scope);
+                    }
+                    $('#lstSearchUIDGroupByName').append(cHtml);
+
                 },
                 error: function(a) {}
             })
         }
     });
 </script>
+<?php
+use luya\web\Svg;
+$spinner = Svg::widget([
+    'folder' => Yii::getAlias("@admin/resources/svg"),
+    'cssClass' => 'svg-spinner',
+    'file' => 'login/spinner.svg'
+]);
+?>
 <div class="luya-content" ng-controller="UidController">
     <div class="row">
         <div class="col-lg-12 uid">
@@ -63,9 +103,14 @@
                                         <div class="input-group mb-2 mr-sm-2 mb-sm-0">
                                             <input class="form-control"
                                                    ng-model="group_name" type="text" placeholder="Tìm theo nhóm tên">
-                                            <div class="input-group-addon" ng-click="findByGroupName()">
-                                                <i class="material-icons">search</i>
+                                            <div class="input-group-addon" style="background: #e5015f;" ng-click="findByGroupName()">
+                                                <span style="border-color: #e5015f; background: #e5015f;" class="login-btn" type="submit"  tabindex="3">
+                                                    <span style="background: #e5015f;color: white;" class="login-spinner"><?= $spinner; ?></span>
+                                                    <i style="background: #e5015f;color: white;    font-size: 19px;" class="material-icons">search</i>
+                                                </span>
+
                                             </div>
+
                                         </div>
                                         <ul style="max-height: 250px;overflow: auto;margin-bottom: -300px;z-index: 9;position: relative;box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.4);" id="lstSearchUIDGroupByName" class="list-group"></ul>
 
